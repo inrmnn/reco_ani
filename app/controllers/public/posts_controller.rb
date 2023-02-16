fe93cb2269e4ce8d4975fd48df83e4d3417d277d
EC2 Default User <ec2-user@ip-172-31-25-79.ap-northeast-1.compute.internal>
2023-02-16 16:23:05 +0000

    投稿機能実装

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,10 +1,23 @@
 class Public::PostsController < ApplicationController
   def new
+    @post = Post.new
   end
-
+  
+  def create
+    @post = Post.new(post_params)
+    @post.save
+  end
+  
   def index
+    @posts = Post.all
   end
 
   def show
+  end
+  
+  private
+  
+  def post_params
+     params.require(:list).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,31 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-  end
-  namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'posts/new'
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :public do
-    get 'users/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :users
+ 
+  devise_for :users, controllers: {
+      registrations: "public/registrations",
+      sessions: 'public/sessions'
+    }
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
       sessions: "admin/sessions"
   }
+  
+  namespace :admin do
+    resources :users, only: [:index, :show]
+   
+    resources :genres, only: [:index, :edit]
+   
+    resources :posts, only: [:index, :show]
+  
+    root to: 'homes#top'
+  end
+  
+  scope module: :public do
+    resources :posts, only: [:new, :create, :index, :show]
+    
+    resources :users, only: [:show]
+    
+    get 'homes/about'
+    
+    root to: 'homes#top'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
